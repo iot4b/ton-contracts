606dd1961ce6427026e8c5fe4088b0c4678ca066
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-07-01 10:59:07 +0700

    device group contract sample

--- deploy.ts
+++ deploy.ts
@@ -1,15 +1,21 @@
 import { resolve } from "path";
 import { readFile } from "fs/promises";
-import { contractAddress } from "@ton/core";
+import {Address, contractAddress, Dictionary} from "@ton/core";
 import { prepareTactDeployment } from "@tact-lang/deployer";
-import { DeviceGroup } from "./output/ton-contracts_DeviceGroup";
+import { DeviceGroup } from "./output/DeviceGroup_DeviceGroup";
 
 async function main() {
     console.log("Deploying...");
     const toProduction = process.argv.length === 3 && process.argv[2] === "mainnet";
-    const init = await DeviceGroup.init();
+    const addr = Address.parse('0QCSES0TZYqcVkgoguhIb8iMEo4cvaEwmIrU5qbQgnN8fo2A');
+    const init = await DeviceGroup.init(
+        addr,
+        "Test Device Group",
+        addr,
+        Dictionary.empty(),
+    );
     const prepare = await prepareTactDeployment({
-        pkg: await readFile(resolve(__dirname, "output", "ton-contracts_DeviceGroup.pkg")),
+        pkg: await readFile(resolve(__dirname, "output", "DeviceGroup_DeviceGroup.pkg")),
         data: init.data.toBoc(),
         testnet: !toProduction,
     });
